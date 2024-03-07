b5c6531fb2211518d0c1af25dc8b4f59b512daff
Thiago Demas <thiago.demas@flinsider.com.br>
2024-03-07 18:52:14 -0300

    Configurações iniciais

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import fastify from 'fastify'
+import { register } from './http/controllers/register'
+import { appRoutes } from './http/routes'
+
+export const app = fastify()
+
+app.register(appRoutes)
