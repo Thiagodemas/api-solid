44e15943ab4c7a7e9bcb5fd02e20408de888e0e8
Thiago Demas <thiago.demas@flinsider.com.br>
2024-03-19 17:42:39 -0300

    Criando a mensagem de error para o UserAlreadyExistsError

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,6 +1,7 @@
 import { prisma } from '@/lib/prisma'
 import { UserRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 interface RegisterServiceRequest {
   name: string
@@ -17,7 +18,7 @@ export class RegisterService {
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw Error('E-mail already exists')
+      throw new UserAlreadyExistsError()
     }
 
     await this.usersRepository.create({
