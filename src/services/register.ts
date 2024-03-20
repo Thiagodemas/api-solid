2593b8eb9e9a33de2d4abb7ce3eb849232d36c35
Thiago Demas <thiago.demas@flinsider.com.br>
2024-03-20 13:01:36 -0300

    Modifica o retorno da função register user

--- src/services/register.ts
+++ src/services/register.ts
@@ -2,6 +2,7 @@ import { prisma } from '@/lib/prisma'
 import { UserRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@prisma/client'
 
 interface RegisterServiceRequest {
   name: string
@@ -9,10 +10,18 @@ interface RegisterServiceRequest {
   password: string
 }
 
+interface RegisterServiceResponse {
+  user: User
+}
+
 export class RegisterService {
   constructor(private usersRepository: UserRepository) {}
 
-  async execute({ name, email, password }: RegisterServiceRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterServiceRequest): Promise<RegisterServiceResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -21,10 +30,14 @@ export class RegisterService {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return {
+      user,
+    }
   }
 }
